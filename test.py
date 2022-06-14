ba90eef7003844d1a4fca6e5a5ba56cef105233d
alaminkawsar <alaminkawsar8643@gmail.com>
2022-06-14 19:50:35 +0600

    test

--- test.py
+++ test.py
@@ -1,35 +1,17 @@
-import scipy.io as sio
-import numpy as np
-import os
-import gc
-import six.moves.urllib as urllib
 import cv2
-import time
-import xml.etree.cElementTree as ET
+import matplotlib.pyplot as plt
+import numpy as np
 import random
-import shutil
-from shutil import copyfile
-import zipfile
+import matplotlib.patches as patches
+from pathlib import Path
 
-import csv
-
-"""
-ORIGINAL SOURCE: https://github.com/molyswu/hand_detection/blob/temp/hand_detection/egohands_dataset_clean.py
-
-I have cleaned up slightly, allowed for python3 functionality and some other things too:
-   > using os.path.join instead of string1 + "/" + string2
-   > ignores bounding boxes in original dataset where xmin = xmax or ymin=ymax
-   > allows to choose a subset of the 40 subfolders of images.
-"""
+'''Image Read'''
+def read_image(path):
+    im = cv2.imread(str(path))
+    print(im.shape)
+    return cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
 
-def download_egohands_dataset(dataset_url, dataset_path):
-    print(os.path.join('1st','2nd'))
-
-if __name__=="__main__":
-
-    DATASET_URL = "INCLUDE_DATASET_URL"
-    DATASET_FILE = "DATASET_NAME.zip"
-    images_dir = 'images'
-
-    download_egohands_dataset(DATASET_URL, DATASET_FILE)
+im = read_image('/home/kawsar/Desktop/Deep Learning/helper-python-scripts/Images/pikachu.jpg')
+plt.imshow(im)
+plt.show()
