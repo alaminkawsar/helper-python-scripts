3c6e50a606e3e51eb287f3667586837cb31130e9
alaminkawsar <alaminkawsar8643@gmail.com>
2022-09-05 01:07:32 +0600

    move file from one to another folder

--- File Processing/move_file_to_a_folder.py
+++ File Processing/move_file_to_a_folder.py
@@ -16,12 +16,11 @@ syntax: shutil.move(old_directory,new_directory)
 '''Type-2'''
 
 #os.listdir() list all of file in a current directory
-images = [f for f in os.listdir() if '.jpg' or '.png' in f.lower()]
+images = [f for f in os.listdir() if ('.jpg' or '.jpeg') in f]
 print(images)
 
 
-os.mkdir('downloaded_images')
-
 for image in images:
-    new_path = 'downloaded_images/' + image
+    new_path = './downloaded_images/' +image
     shutil.move(image, new_path)
+    print(new_path)
