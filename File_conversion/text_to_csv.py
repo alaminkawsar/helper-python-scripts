47c64938c17a7dc45a0ec608f2a10187ff657c21
alaminkawsar <alaminkawsar8643@gmail.com>
2022-09-05 01:07:42 +0600

    convert text to csv file

--- File_conversion/text_to_csv.py
+++ File_conversion/text_to_csv.py
@@ -4,42 +4,106 @@ from turtle import heading
 import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
+import shutil
 
 
+img_directory_path='/content/drive/MyDrive/tv+coco_hand/TV-Hand/TV-Hand_Images'
+test_img_dir = '/content/drive/MyDrive/tv+coco_hand/images/test'
 
-path = '/home/kawsar/Desktop/Deep Learning/helper-python-scripts'
+text_file_path = '/content/drive/MyDrive/tv+coco_hand/TV-Hand'
+text_file_name = "test_IDs.txt"
+
+add_path = 'images/test/'
+
+csv_file_path = '/content/drive/MyDrive/tv+coco_hand/images/test'
+csv_file_name = 'test_labels.csv'
+
 list2d = []
-img_path='/home/kawsar/Desktop/Deep Learning/helper-python-scripts/Images'
 indx = 0
 list2d.append(['filename','width','height','class','xmin','ymin','xmax','ymax'])
+
 listSize = {
     
 }
-filepath = os.listdir(img_path)
+
+k = 0
+filepath = os.listdir(img_directory_path)
 for x in filepath:
-    img = Image.open(os.path.join(img_path,x))
+    img = Image.open(os.path.join(img_directory_path,x))
     width, height = str(img.width), str(img.height)
     listSize[x] = [width, height]
+    #print(x, listSize[x])
 
-with open(os.path.join(path,"text_file.txt"), "r") as f:
+
+#please comment here
+    # if k ==10:
+    #   break
+    # k +=1
+
+#pleae comment here
+# listSize['000074_006.jpg'] = ['100','100']
+# listSize['000204_005.jpg'] = ['100','100']
+
+copy_img_list = {
+    
+}
+k = 0
+with open(os.path.join(text_file_path,text_file_name), "r") as f:
     for line in f.readlines():
         if line[-1]=='\n' :
             line = line[:-1]
         new_val = line.split(',')
-        
-        if new_val[0] in listSize.keys():
-            width_height = listSize[new_val[0]]
-        else:
-            continue
-        
-        new_val[2], new_val[3] = new_val[3], new_val[2]
-        new_val = ['images/test/'+new_val[0]] + width_height+ ['hand'] + new_val[1:5]
-        
-        #print(new_val)
-        list2d.append(new_val)
+        img_name_path = new_val[0]
+
+        #process xmin,ymin,xmax,ymax
+        find_val = list(map(int,new_val[1:5]))
+        find_val[1], find_val[2] = find_val[2], find_val[1]
+
+        #process name
+        img_name = img_name_path.split('/')[-1]
+        img_file_path = [add_path + img_name]
+
+        if '-1' in find_val:
+          continue
+
+        if img_name in listSize.keys():
+          v = img_file_path + listSize[img_name] + ['hand'] + find_val
+          print(v)
+          
+          copy_img_list[img_name] = 1
+          list2d.append(v)
+
+        #please comment
+        # else:
+        #   print(img_name, find_val)
+
+        # k += 1
+        # if k==20: 
+        #   break
 
 
-with open(os.path.join(path,'images/test/test.csv'), 'w', newline='') as file:
+with open(os.path.join(csv_file_path,csv_file_name), 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(list2d)
+
+#move image file into test/train directory
+k = 0
+check_exist = {}
+
+for x in filepath:
+    img = Image.open(os.path.join(img_directory_path,x))
+    old_dir = os.path.join(img_directory_path,x)
+    new_dir = os.path.join(test_img_dir,x)
+    if new_dir in check_exist.keys():
+      continue
+    #print(old_dir, new_dir)
+    if x in copy_img_list.keys():
+      shutil.copyfile(old_dir,new_dir)
+      check_exist[x] = True
+
     
+    # print(x)
+    # if k ==10:
+    #   break
+    # k +=1
+print('successfully shifted and created csv file')
