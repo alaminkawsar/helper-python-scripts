87f0ab0beb8fba6ffe0283d3ba3bc749738da596
alaminkawsar <alaminkawsar8643@gmail.com>
2023-06-17 08:08:20 +0600

    delete unnecessary file

--- downloaded_images/count_files.py
+++
@@ -1,5 +0,0 @@
-import os
-dir = '/content/drive/MyDrive/CocoHandResnet50/COCO-Hand/COCO-Hand-S/COCO-Hand-S_Images'
-list = os.listdir(dir) # dir is your directory path
-number_files = len(list)
-print (number_files)
